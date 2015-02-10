53ee7f2ef0ec037a6c18621d2aedc742da6b2c4c
28246le <lee.levett@yell.com>
2015-02-10 10:57:26 +0000

    First mocking. Changed mockito version due to incompatibility with powermock.

--- legacytesting/src/main/java/legacycode/LegacyCode.java
+++ legacytesting/src/main/java/legacycode/LegacyCode.java
@@ -2,7 +2,7 @@ package legacycode;
 
 public class LegacyCode {
   private static final boolean conditionalAvailable = ConditionalSingleton.getInstance().isAvailable();
-  private static final boolean property = PropertyHolder.getInstance().getProperty();
+  private static final boolean propertyTrue = PropertyHolder.getInstance().getProperty();
   private final LegacyIdProvider idProvider;
 
   public LegacyCode() {
@@ -10,6 +10,21 @@ public class LegacyCode {
   }
 
   public void doLegacyOperation() {
+    if (conditionalAvailable && propertyTrue) {
+      if (idProvider.getId().equals("12345")) {
+        doThis();
+      } else {
+        doThat();
+      }
+    }
+  }
+
+  private void doThis() {
+    System.out.println("Don't do this");
+  }
+
+  private void doThat() {
+    System.out.println("Do that");
 
   }
 }
--- legacytesting/src/test/java/legacycode/LegacyCodeTest.java
+++ legacytesting/src/test/java/legacycode/LegacyCodeTest.java
@@ -1,9 +1,15 @@
 package legacycode;
 
 import static org.junit.Assert.*;
+import static org.powermock.api.mockito.PowerMockito.mock;
+import static org.powermock.api.mockito.PowerMockito.when;
+import static org.powermock.api.mockito.PowerMockito.whenNew;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.powermock.modules.junit4.PowerMockRunner;
 
+@RunWith(PowerMockRunner.class)
 public class LegacyCodeTest {
 
   // Availability monitor (ConditionalSingleton) *only* starts up without exception if props file is present.
@@ -11,8 +17,23 @@ public class LegacyCodeTest {
   // With a rule. Maybe? For some reason not in the actual code though?
 
   @Test
-  public void test() {
-    fail("Not yet implemented");
+  public void test() throws Exception {
+    LegacyCode legacy = givenNewLegacyCodeObjectWithId();
+    whenLegacyOperationIsPerformed(legacy);
+
+    // verify doThat & inner method called.
   }
 
+  private void whenLegacyOperationIsPerformed(LegacyCode legacy) {
+    legacy.doLegacyOperation();
+  }
+
+  private LegacyCode givenNewLegacyCodeObjectWithId() throws Exception {
+    // 1. mock id provider. Constructors.
+    // 2. PowerMockRunner
+    final LegacyIdProvider idProvider = mock(LegacyIdProvider.class);
+    when(idProvider.getId()).thenReturn("98765");
+    whenNew(LegacyIdProvider.class).withAnyArguments().thenReturn(idProvider);
+    return new LegacyCode();
+  }
 }
