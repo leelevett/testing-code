b7cd661dcbea2c0248646e2435d384a329e597fb
28246le <lee.levett@yell.com>
2015-02-17 17:43:50 +0000

    Green test... Sort of ;)

--- legacytesting/src/main/java/legacycode/ConditionalSingleton.java
+++ legacytesting/src/main/java/legacycode/ConditionalSingleton.java
@@ -3,7 +3,8 @@ package legacycode;
 public class ConditionalSingleton {
   private ConditionalSingleton() {
     // Simulate some legacy code behaviour.
-    if (this.getClass().getClassLoader().getResource("conditional.singleton.properties") == null) {
+    ClassLoader cl = ClassLoader.getSystemClassLoader();
+    if (cl.getResource("conditional.singleton.properties") == null) {
       throw new InstantiationError(
           "ConditionalSingleton couldn't be created: Didn't find conditional.singleton.properties");
     }
--- legacytesting/src/test/java/legacycode/LegacyCodeTest.java
+++ legacytesting/src/test/java/legacycode/LegacyCodeTest.java
@@ -5,16 +5,37 @@ import static org.powermock.api.mockito.PowerMockito.mock;
 import static org.powermock.api.mockito.PowerMockito.when;
 import static org.powermock.api.mockito.PowerMockito.whenNew;
 
+import org.apache.commons.io.FileUtils;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.powermock.modules.junit4.PowerMockRunner;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 @RunWith(PowerMockRunner.class)
 public class LegacyCodeTest {
+  // 3. We can not use @Rule TemporaryFolder as this is properties and we *need* it to be in "resources". Not tmp.
+  // Hence this fudging.
+  public static Path resourcePath;
 
-  // Availability monitor (ConditionalSingleton) *only* starts up without exception if props file is present.
-  // We only need the thing there. We don't care what's in it. So we'll fake it
-  // With a rule. Maybe? For some reason not in the actual code though?
+  @BeforeClass
+  public static void setUp() throws IOException {
+    // 4. note: test-classes. That took an age to figure out. For when you haven't got anywhere to put resources to be compiled into test-classes.
+    resourcePath = Paths.get("target/test-classes/conditional.singleton.properties");
+    Files.createFile(resourcePath);
+  }
+  @AfterClass
+  public static void tearDown() throws IOException {
+    if(Files.exists(resourcePath)){
+      FileUtils.deleteQuietly(resourcePath.toFile());
+    }
+  }
 
   @Test
   public void test() throws Exception {
