934ee84f8e4e9fc1073aaa043f510a2ca3cd4f95
28246le <lee.levett@yell.com>
2015-07-15 09:10:38 +0100

    Fully working

--- bashProject/src/main/bin/read-props.sh
+++ bashProject/src/main/bin/read-props.sh
@@ -1,15 +1,10 @@
 #!/usr/bin/env bash
 
-function readVariablesFromProperties() {
-  while read -r line;
-  do
-    if [[ $line =~ \= ]]; then
-      rhs=${line#*=}
-      lhs=${line%=*}
-      export $lhs="$rhs"
-    fi
-  done < <(curl --silent --get http://some.server.co.uk/props)
-}
-
-readVariablesFromProperties
-  echo "$prop1|$prop2"
+while read -r line;
+do
+  if [[ $line =~ \= ]]; then
+    rhs=${line#*=}
+    lhs=${line%=*}
+    export $lhs="$rhs"
+  fi
+done < <(curl --silent --get http://some.server.co.uk/props)
--- bashProject/src/test/bin/prop-service-test.sh
+++ bashProject/src/test/bin/prop-service-test.sh
@@ -4,11 +4,16 @@ setUp()
 {
   MAIN_DIR=../../main
   PATH=$MAIN_DIR/bin:$PATH
+
+  cp ./mock-curl.sh $MAIN_DIR/bin/curl
 }
 
 tearDown()
 {
-  echo "tearing down"
+  rm $MAIN_DIR/bin/curl
+
+  unset prop1
+  unset prop2
 }
 
 testPropertyService()
@@ -16,8 +21,6 @@ testPropertyService()
   cd $MAIN_DIR/bin && \
   source read-props.sh && \
   cd - &> /dev/null
-
-  echo "$prop1:$prop2"
 
   assertEquals "prop1 is not set correctly" "a property" "$prop1"
   assertEquals "prop2 is not set correctly" "A second property!" "$prop2"
